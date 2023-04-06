9fb5d7609cd20bafe17a9589c57d5633a6e9b8b2
Fernando Souza <93844918+FernandoBMSouza@users.noreply.github.com>
2023-04-06 13:51:39 -0300

    Mudando o algoritmo

--- Program.cs
+++ Program.cs
@@ -1,47 +1,39 @@
-﻿int[] a = new int[] { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
-quickSort(a);
+﻿int[] a = new int[] { 7, 9, 2, 5, 1, 10, 4, 8, 6, 3 };
+QuickSort(a, 0, a.Length - 1);
 for (int i = 0; i < a.Length; i++)
 {
     System.Console.Write(a[i] + " ");
 }
 
-
-void quickSort(int[] vetor)
+void QuickSort(int[] a, int p, int r)
 {
-    int inicio = 0;
-    int fim = vetor.Length - 1;
-
-    QuickSort(vetor, inicio, fim);
+    if (p < r) //se posição inicial for menor que final
+    {
+        int q = Partition(a, p, r);
+        QuickSort(a, p, q - 1); //chamando quicksort do lado esquerdo do pivot
+        QuickSort(a, q + 1, r); //chamando quicksort do lado direito do pivot
+    }
 }
 
-void QuickSort(int[] vetor, int inicio, int fim)
+int Partition(int[] a, int p, int r)
 {
-    if (inicio < fim)
+    int x = a[r]; //x recebe o último elemento do vetor setando como pivot
+    int i = p - 1; //y recebe o index da posição inicial menos 1
+    for (int j = p; j < r; j++)
     {
-        int p = vetor[inicio];
-        int i = inicio + 1;
-        int f = fim;
-
-        while (i <= f)
+        if (a[j] <= x)
         {
-            if (vetor[i] <= p)
-                i++;
-            else if (p < vetor[f])
-                f--;
-            else
-            {
-                int aux = vetor[i];
-                vetor[i] = vetor[f];
-                vetor[f] = aux;
-                i++;
-                f--;
-            }
+            i++;
+            Swap(a, i, j);
         }
-
-        vetor[inicio] = vetor[f];
-        vetor[f] = p;
-
-        QuickSort(vetor, inicio, f - 1);
-        QuickSort(vetor, f + 1, fim);
     }
+    Swap(a, i + 1, r);
+    return i + 1;
+}
+
+void Swap(int[] a, int p1, int p2)
+{
+    int aux = a[p1];
+    a[p1] = a[p2];
+    a[p2] = aux;
 }
